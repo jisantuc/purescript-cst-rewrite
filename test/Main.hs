af67430736a3a428c47dd486cb6f73b9ac384353
James Santucci <james.santucci@gmail.com>
2021-05-16 11:01:59 -0600

    now actually run the test

--- test/Main.hs
+++ test/Main.hs
@@ -6,7 +6,7 @@ import Language.PureScript.CST.Parser (parseModule)
 import Data.CSTRewrite (rewrite)
 
 main :: IO ()
-main = print "you should write some tests"
+main = testImportRewrite
 
 testImportRewrite :: IO ()
 testImportRewrite = do
